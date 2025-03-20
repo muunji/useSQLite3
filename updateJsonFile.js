6efc696cc7566c3b6e886778650d2568c6cee087
김민지 <qniomo00@gmail.com>
2025-03-20 15:50:30 +0900

    json파일로 저장하는 함수 생성

--- updateJsonFile.js
+++ updateJsonFile.js
@@ -1,3 +1,26 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /**
  * 데이터베이스에서 데이터를 읽어와서 JSON파일로 저장하는 함수
  */
+
+export function updateJsonFile() {
+  db.all(`SELECT * FROM test`, (err, rows) => {
+    if (err) {
+      console.error('DB 조회 실패', err.message)
+      return
+    }
+  })
+  fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(rows), 'utf-8', (err) => {
+    if (err) {
+      console.error('JSON 파일 저장 실패', err.message)
+      return
+    }
+    console.log('데이터가 JSON파일로 저장됨')
+  })
+}
