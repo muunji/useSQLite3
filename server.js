bc308a70dc2c41ef30fde6cc0355b70b079a3fe5
김민지 <qniomo00@gmail.com>
2025-03-20 15:54:27 +0900

    함수로 생성 > /json 경로 필요없음

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+import { updateJsonFile } from './updateJsonFile.js'
+
 const db = new sqlite3.Database('test.db', (err) => {
   if (err) {
     console.log('DB 연결 실패', err.message)
@@ -28,25 +30,27 @@ app.post('/save', (req, res) => {
       res.status(500).send('DB 저장 실패')
       return
     }
+    updateJsonFile(db)
     res.json({ id: this.lastID })
+
   })
 })
 
 //데이터베이스를 JSON파일로 저장하는 API 추가
-app.get('/json', (req, res) => {
-  db.all(`SELECT * FROM test`, (err, rows) => {
-    if (err) {
-      console.error('DB 조회 실패', err.message)
-      res.status(500).send('DB 조회 실패')
-      return
-    }
+// app.get('/json', (req, res) => {
+//   db.all(`SELECT * FROM test`, (err, rows) => {
+//     if (err) {
+//       console.error('DB 조회 실패', err.message)
+//       res.status(500).send('DB 조회 실패')
+//       return
+//     }
 
-    //데이터를 JSON 형식으로 변환하여 파일로 저장
-    fs.writeFileSync('test.json', JSON.stringify(rows), 'utf-8')
-    console.log('데이터가 JSON파일로 저장됨')
+//     //데이터를 JSON 형식으로 변환하여 파일로 저장
+//     fs.writeFileSync('test.json', JSON.stringify(rows), 'utf-8')
+//     console.log('데이터가 JSON파일로 저장됨')
 
-    res.json({message : 'JSON 파일 저장 완료'})
-  })
-})
+//     res.json({message : 'JSON 파일 저장 완료'})
+//   })
+// })
 
 app.listen(3010, ()=>{console.log('DB : http://localhost:3010')})
--- updateJsonFile.js
+++ updateJsonFile.js
@@ -9,18 +9,18 @@ const __dirname = path.dirname(__filename)
  * 데이터베이스에서 데이터를 읽어와서 JSON파일로 저장하는 함수
  */
 
-export function updateJsonFile() {
+export function updateJsonFile(db) {
   db.all(`SELECT * FROM test`, (err, rows) => {
     if (err) {
       console.error('DB 조회 실패', err.message)
       return
     }
-  })
-  fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(rows), 'utf-8', (err) => {
-    if (err) {
-      console.error('JSON 파일 저장 실패', err.message)
-      return
-    }
-    console.log('데이터가 JSON파일로 저장됨')
+    fs.writeFileSync(path.join(__dirname, 'test.json'), JSON.stringify(rows), 'utf-8', (err) => {
+      if (err) {
+        console.error('JSON 파일 저장 실패', err.message)
+        return
+      }
+      console.log('데이터가 JSON파일로 저장됨')
+    })
   })
 }
