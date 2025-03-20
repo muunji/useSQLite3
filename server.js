b760467f0b6966f10dcb78c26bbd3e8080537fda
김민지 <qniomo00@gmail.com>
2025-03-20 15:31:11 +0900

    변수 수정과 주석으로 설명

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ const db = new sqlite3.Database('test.db', (err) => {
   console.log('DB 연결 성공')
 })
 
+//데이터 저장 API
 app.post('/save', (req, res) => {
-  const data = req.body
+  const {text, isCheck, number} = req.body
   console.log('data', data)
 
-  // isCheck 값이 undefined 또는 null인 경우 기본값 설정
-  const isCheckValue = data.isCheck !== 'on' ? 0 : 1
-  const numberValue = data.number ? parseFloat(data.number) : null
+  const isCheckValue = isCheck !== 'on' ? 0 : 1
+  const numberValue = number ? parseFloat(number) : null
   
-  db.run(`INSERT INTO test (text, isCheck,number) VALUES (?, ?,?)`, [data.text, isCheckValue,numberValue], function(err) {
+  db.run(`INSERT INTO test (text, isCheck,number) VALUES (?, ?,?)`, [text, isCheckValue,numberValue], function(err) {
     if (err) {
       console.error('DB 저장 실패:', err.message) // 에러 메시지 출력e) // 에러 메시지 출력
       res.status(500).send('DB 저장 실패')
@@ -31,5 +31,8 @@ app.post('/save', (req, res) => {
     res.json({ id: this.lastID })
   })
 })
+
+//데이터베이스를 JSON파일로 저장하는 API 추가
+
 
 app.listen(3010, ()=>{console.log('DB : http://localhost:3010')})
